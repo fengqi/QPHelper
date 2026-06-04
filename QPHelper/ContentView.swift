79349c15119b440b51baa535b55db2403cbba292
fengqi <lyf362345@gmail.com>
2026-06-04 11:17:34 +0800

    Initial Commit

---
+++ QPHelper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QPHelper
+//
+//  Created by liaoyongfa on 2026/6/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QPHelper/QPHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  QPHelperApp.swift
+//  QPHelper
+//
+//  Created by liaoyongfa on 2026/6/4.
+//
+
+import SwiftUI
+
+@main
+struct QPHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
